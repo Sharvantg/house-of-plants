c1cbb0900ac687730230ee8e99ad23e5d31e4517
Sharvan TG <sharvantg@hotmail.com>
2024-03-19 08:53:20 +0530

    Removed a component folder

---
+++ .webflowrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  host: "https://api.webflow.com",
+  rootDir: "./devlink",
+  siteId: "65f5a1e5ea332f6db536e296",
+  authToken: "4aaa3c6c912406668b0a2dd4f0b5f3f0a9250effe7af3ee76e3b3a6cc9402d9b", // An environment variable is recommended for this field.
+  cssModules: false,
+  skipTagSelectors: false,
+};
--- Components/MainContainer.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useState } from "react";
-
-function MainContainer({ children }) {
-  return <div className="mainContainer">{children}</div>;
-}
-
-export default MainContainer;
